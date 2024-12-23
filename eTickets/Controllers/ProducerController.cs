a9eaed0c7eedbc9b44fc81bab594c5601870eaae
Batuhan024 <batuhantest2001@gmail.com>
2024-12-24 00:33:56 +0300

    24.12

--- eTickets/Controllers/ProducerController.cs
+++ eTickets/Controllers/ProducerController.cs
@@ -15,7 +15,7 @@ namespace eTickets.Controllers
         public async Task<IActionResult> Index()
         {
             var allProducer = await _context.Producers.ToListAsync();
-            return View();
+            return View(allProducer);
         }
     }
 }
--- eTickets/Program.cs
+++ eTickets/Program.cs
@@ -1,4 +1,5 @@
 using eTickets.Data;
+using eTickets.Data.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +10,7 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<IActorsService, ActorsService>();
 
 var app = builder.Build();
 
